7b5056125655459a278c46d6b80bbd891a2468bf
Scott Cytacki <scytacki@concord.org>
2019-09-03 14:43:01 -0400

    add size text and update when the interactive is resized

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -7,11 +7,22 @@ interface IState {}
 
 export class Text extends React.Component<IProps, IState> {
   public render() {
+    const aspectRatio = ( window.innerWidth / window.innerHeight ).toFixed(3);
     const { text } = this.props;
     return (
       <div>
-        {text}
+        Width: {window.innerWidth}px<br/>
+        Height: {window.innerHeight}px<br/>
+        Aspect Ratio (width /height): {aspectRatio}
       </div>
     );
+  }
+
+  public componentDidMount() {
+    window.addEventListener("resize", this.updateSizeText.bind(this));
+  }
+
+  private updateSizeText(event: UIEvent) {
+    this.forceUpdate();
   }
 }
